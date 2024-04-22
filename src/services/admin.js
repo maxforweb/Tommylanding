ced2eb3942262f9f71a6a79a49634920c8f8f10b
Serhii Son <serhii@artworking.co.uk>
2024-04-22 10:32:51 +0100

    add build folder. change api endpoint

--- src/services/admin.js
+++ src/services/admin.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const adminApi = createApi({
 	reducerPath: "adminApi",
-	baseQuery: fetchBaseQuery({ baseUrl: "https://admin.prodtest1.space/api/site-data" }),
+	baseQuery: fetchBaseQuery({ baseUrl: "https://aws.tomocat.com/api/site-data" }),
 	endpoints: (builder) => ({
 		getSitInfo: builder.query({
 			query: () => `/`,
